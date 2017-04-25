ce28878fb67d700ef8e268ae8b79f365dd89e9dc
markosconde <markrdguez@hotmail.com>
2017-04-25 11:33:23 +0200

    1.01

--- Notepad++/MyNotepad.Designer.vb
+++ Notepad++/MyNotepad.Designer.vb
@@ -22,6 +22,7 @@ Partial Class MyNotepad
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MyNotepad))
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.FileTool = New System.Windows.Forms.ToolStripMenuItem()
         Me.NewTool = New System.Windows.Forms.ToolStripMenuItem()
@@ -41,8 +42,22 @@ Partial Class MyNotepad
         Me.FindReplaceTool = New System.Windows.Forms.ToolStripMenuItem()
         Me.helpToolStrip = New System.Windows.Forms.ToolStripMenuItem()
         Me.AboutTool = New System.Windows.Forms.ToolStripMenuItem()
-        Me.texto = New System.Windows.Forms.RichTextBox()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.NewToolStripButton = New System.Windows.Forms.ToolStripButton()
+        Me.OpenToolStripButton = New System.Windows.Forms.ToolStripButton()
+        Me.SaveToolStripButton = New System.Windows.Forms.ToolStripButton()
+        Me.toolStripSeparator = New System.Windows.Forms.ToolStripSeparator()
+        Me.CutToolStripButton = New System.Windows.Forms.ToolStripButton()
+        Me.CopyToolStripButton = New System.Windows.Forms.ToolStripButton()
+        Me.PasteToolStripButton = New System.Windows.Forms.ToolStripButton()
+        Me.toolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripundo = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripredo = New System.Windows.Forms.ToolStripButton()
+        Me.TabControl = New System.Windows.Forms.TabControl()
+        Me.tpMas = New System.Windows.Forms.TabPage()
         Me.MenuStrip1.SuspendLayout()
+        Me.ToolStrip1.SuspendLayout()
+        Me.TabControl.SuspendLayout()
         Me.SuspendLayout()
         '
         'MenuStrip1
@@ -189,27 +204,135 @@ Partial Class MyNotepad
         Me.AboutTool.Size = New System.Drawing.Size(107, 22)
         Me.AboutTool.Text = "About"
         '
-        'texto
+        'ToolStrip1
         '
-        Me.texto.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.texto.Location = New System.Drawing.Point(0, 24)
-        Me.texto.Name = "texto"
-        Me.texto.Size = New System.Drawing.Size(686, 545)
-        Me.texto.TabIndex = 2
-        Me.texto.Text = ""
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripButton, Me.OpenToolStripButton, Me.SaveToolStripButton, Me.toolStripSeparator, Me.CutToolStripButton, Me.CopyToolStripButton, Me.PasteToolStripButton, Me.toolStripSeparator2, Me.ToolStripundo, Me.ToolStripredo})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 24)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(686, 25)
+        Me.ToolStrip1.TabIndex = 3
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'NewToolStripButton
+        '
+        Me.NewToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.NewToolStripButton.Image = CType(resources.GetObject("NewToolStripButton.Image"), System.Drawing.Image)
+        Me.NewToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.NewToolStripButton.Name = "NewToolStripButton"
+        Me.NewToolStripButton.Size = New System.Drawing.Size(23, 22)
+        Me.NewToolStripButton.Text = "&New"
+        '
+        'OpenToolStripButton
+        '
+        Me.OpenToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.OpenToolStripButton.Image = CType(resources.GetObject("OpenToolStripButton.Image"), System.Drawing.Image)
+        Me.OpenToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.OpenToolStripButton.Name = "OpenToolStripButton"
+        Me.OpenToolStripButton.Size = New System.Drawing.Size(23, 22)
+        Me.OpenToolStripButton.Text = "&Open"
+        '
+        'SaveToolStripButton
+        '
+        Me.SaveToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.SaveToolStripButton.Image = CType(resources.GetObject("SaveToolStripButton.Image"), System.Drawing.Image)
+        Me.SaveToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.SaveToolStripButton.Name = "SaveToolStripButton"
+        Me.SaveToolStripButton.Size = New System.Drawing.Size(23, 22)
+        Me.SaveToolStripButton.Text = "&Save"
+        '
+        'toolStripSeparator
+        '
+        Me.toolStripSeparator.Name = "toolStripSeparator"
+        Me.toolStripSeparator.Size = New System.Drawing.Size(6, 25)
+        '
+        'CutToolStripButton
+        '
+        Me.CutToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.CutToolStripButton.Image = CType(resources.GetObject("CutToolStripButton.Image"), System.Drawing.Image)
+        Me.CutToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.CutToolStripButton.Name = "CutToolStripButton"
+        Me.CutToolStripButton.Size = New System.Drawing.Size(23, 22)
+        Me.CutToolStripButton.Text = "C&ut"
+        '
+        'CopyToolStripButton
+        '
+        Me.CopyToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.CopyToolStripButton.Image = CType(resources.GetObject("CopyToolStripButton.Image"), System.Drawing.Image)
+        Me.CopyToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.CopyToolStripButton.Name = "CopyToolStripButton"
+        Me.CopyToolStripButton.Size = New System.Drawing.Size(23, 22)
+        Me.CopyToolStripButton.Text = "&Copy"
+        '
+        'PasteToolStripButton
+        '
+        Me.PasteToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.PasteToolStripButton.Image = CType(resources.GetObject("PasteToolStripButton.Image"), System.Drawing.Image)
+        Me.PasteToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.PasteToolStripButton.Name = "PasteToolStripButton"
+        Me.PasteToolStripButton.Size = New System.Drawing.Size(23, 22)
+        Me.PasteToolStripButton.Text = "&Paste"
+        '
+        'toolStripSeparator2
+        '
+        Me.toolStripSeparator2.Name = "toolStripSeparator2"
+        Me.toolStripSeparator2.Size = New System.Drawing.Size(6, 25)
+        '
+        'ToolStripundo
+        '
+        Me.ToolStripundo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripundo.Image = Global.Notepad__.My.Resources.Resource.undo_1_icon_icons_com_62895
+        Me.ToolStripundo.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripundo.Name = "ToolStripundo"
+        Me.ToolStripundo.Size = New System.Drawing.Size(23, 22)
+        Me.ToolStripundo.Text = "ToolStripButton1"
+        '
+        'ToolStripredo
+        '
+        Me.ToolStripredo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripredo.Image = Global.Notepad__.My.Resources.Resource.redo_128
+        Me.ToolStripredo.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripredo.Name = "ToolStripredo"
+        Me.ToolStripredo.Size = New System.Drawing.Size(23, 22)
+        Me.ToolStripredo.Text = "ToolStripButton2"
+        '
+        'TabControl
+        '
+        Me.TabControl.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TabControl.Controls.Add(Me.tpMas)
+        Me.TabControl.Location = New System.Drawing.Point(0, 49)
+        Me.TabControl.Name = "TabControl"
+        Me.TabControl.SelectedIndex = 0
+        Me.TabControl.Size = New System.Drawing.Size(686, 522)
+        Me.TabControl.TabIndex = 4
+        '
+        'tpMas
+        '
+        Me.tpMas.Location = New System.Drawing.Point(4, 22)
+        Me.tpMas.Name = "tpMas"
+        Me.tpMas.Padding = New System.Windows.Forms.Padding(3)
+        Me.tpMas.Size = New System.Drawing.Size(678, 496)
+        Me.tpMas.TabIndex = 1
+        Me.tpMas.Text = "    +"
+        Me.tpMas.UseVisualStyleBackColor = True
         '
         'MyNotepad
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(686, 569)
-        Me.Controls.Add(Me.texto)
+        Me.Controls.Add(Me.TabControl)
+        Me.Controls.Add(Me.ToolStrip1)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "MyNotepad"
         Me.Text = "Notepadd++"
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.TabControl.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -233,6 +356,18 @@ Partial Class MyNotepad
     Friend WithEvents FindReplaceTool As ToolStripMenuItem
     Friend WithEvents helpToolStrip As ToolStripMenuItem
     Friend WithEvents AboutTool As ToolStripMenuItem
-    Friend WithEvents texto As RichTextBox
     Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents NewToolStripButton As ToolStripButton
+    Friend WithEvents OpenToolStripButton As ToolStripButton
+    Friend WithEvents SaveToolStripButton As ToolStripButton
+    Friend WithEvents toolStripSeparator As ToolStripSeparator
+    Friend WithEvents CutToolStripButton As ToolStripButton
+    Friend WithEvents CopyToolStripButton As ToolStripButton
+    Friend WithEvents PasteToolStripButton As ToolStripButton
+    Friend WithEvents toolStripSeparator2 As ToolStripSeparator
+    Friend WithEvents ToolStripundo As ToolStripButton
+    Friend WithEvents ToolStripredo As ToolStripButton
+    Friend WithEvents tpMas As TabPage
+    Friend WithEvents TabControl As TabControl
 End Class
--- Notepad++/MyNotepad.vb
+++ Notepad++/MyNotepad.vb
@@ -1,43 +1,40 @@
 ﻿Public Class MyNotepad
-    Private contenido As New Contenido()
+
+    Dim ListaArchivos As New List(Of Contenido)
+
     Private Sub NewTool_Click(sender As Object, e As EventArgs) Handles NewTool.Click
-        If (contenido.modificado = True) Then
-            Dim resultado As MsgBoxResult = MsgBox("El archivo se ha modificado, ¿Desea guardarlo antes de salir?", vbCritical + vbYesNoCancel)
-            If (resultado = vbYes) Then
-                SaveTool_Click(Nothing, Nothing)
-                texto.Clear()
-                contenido.modificado = False
-                contenido = New Contenido()
-            ElseIf (resultado = vbNo) Then
-                texto.Clear()
-                contenido.modificado = False
-                contenido = New Contenido()
-            End If
-        End If
+        Dim myTabPage As New TabPage()
+        Dim tb As New RichTextBox()
+        AddHandler tb.TextChanged, AddressOf texto_TextChanged
+        myTabPage.Controls.Add(tb)
+        tb.Dock = DockStyle.Fill
+
+        myTabPage.Text = "New" & (TabControl.TabPages.Count)
+        TabControl.TabPages.Insert(TabControl.TabPages.Count - 1, myTabPage)
+        TabControl.SelectedTab = myTabPage
+        Dim c As New Contenido
+        c.modificado = False
+        ListaArchivos.Add(c)
 
     End Sub
 
 
     Private Sub OpenTool_Click(sender As Object, e As EventArgs) Handles OpenTool.Click
 
-        If (contenido.modificado = True) Then
-            Dim resultado As MsgBoxResult = MsgBox("El archivo se ha modificado, ¿Desea guardarlo antes de salir?", vbCritical + vbYesNoCancel)
-            If (resultado = vbYes) Then
-                SaveTool_Click(Nothing, Nothing)
-            End If
-        Else
-            Dim OpenFileDialog As New OpenFileDialog
+
+        Dim OpenFileDialog As New OpenFileDialog
             OpenFileDialog.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.MyDocuments
             OpenFileDialog.Filter = "text Files|*.txt"
-            'Aqui se selecciona el tipo de documento que se podra escoger al buscar
+        'Aqui se selecciona el tipo de documento que se podra escoger al buscar
 
-            If (OpenFileDialog.ShowDialog() = DialogResult.OK) Then
-                contenido.filename = OpenFileDialog.FileName
-                Me.texto.LoadFile(contenido.filename, RichTextBoxStreamType.PlainText)
-                'se carga el contenido del documento de texto y lo muestra
-                Me.Text = "Notepadd++ - " + contenido.filename
-
-            End If
+        If (OpenFileDialog.ShowDialog() = DialogResult.OK) Then
+            ListaArchivos.Item(TabControl.SelectedIndex).filename = OpenFileDialog.FileName
+            For Each c As Control In TabControl.SelectedTab.Controls
+                CType(c, RichTextBox).LoadFile(ListaArchivos.Item(TabControl.SelectedIndex).filename, RichTextBoxStreamType.PlainText)
+            Next
+            'se carga el contenido del documento de texto y lo muestra
+            Me.Text = "Notepadd++ - " + ListaArchivos(TabControl.SelectedIndex).filename
+            TabControl.SelectedTab.Text = ListaArchivos(TabControl.SelectedIndex).filename
         End If
 
 
@@ -45,12 +42,15 @@
 
 
     Private Sub SaveTool_Click(sender As Object, e As EventArgs) Handles SaveTool.Click
-        If Not (contenido.filename = String.Empty) Then
-            Me.texto.SaveFile(contenido.filename, RichTextBoxStreamType.PlainText)
+        If Not (ListaArchivos.Item(TabControl.SelectedIndex).filename = String.Empty) Then
+            For Each g As Control In TabControl.SelectedTab.Controls
+                CType(g, RichTextBox).SaveFile(ListaArchivos.Item(TabControl.SelectedIndex).filename, RichTextBoxStreamType.PlainText)
+                TabControl.SelectedTab.Text = ListaArchivos(TabControl.SelectedIndex).filename
+            Next
         Else
             SaveAsTool_Click(sender, e)
         End If
-        contenido.modificado = False
+        ListaArchivos(TabControl.SelectedIndex).modificado = False
 
     End Sub
 
@@ -61,10 +61,13 @@
         SaveFileDialog.Filter = "Text Files| *.txt*"
 
         If (SaveFileDialog.ShowDialog() = DialogResult.OK) Then
-            contenido.filename = SaveFileDialog.FileName
-            Me.texto.SaveFile(contenido.filename, RichTextBoxStreamType.PlainText)
-            contenido.modificado = False
-            Me.Text = "Notepadd++ - " + contenido.filename
+            ListaArchivos.Item(TabControl.SelectedIndex).filename = SaveFileDialog.FileName
+            For Each g As Control In TabControl.SelectedTab.Controls
+                CType(g, RichTextBox).SaveFile(ListaArchivos.Item(TabControl.SelectedIndex).filename, RichTextBoxStreamType.PlainText)
+            Next
+            ListaArchivos(TabControl.SelectedIndex).modificado = False
+            Me.Text = "Notepadd++ - " + ListaArchivos(TabControl.SelectedIndex).filename
+            TabControl.SelectedTab.Text = ListaArchivos(TabControl.SelectedIndex).filename
 
         End If
 
@@ -75,23 +78,51 @@
 
     End Sub
 
-    Private Sub texto_TextChanged(sender As Object, e As EventArgs) Handles texto.TextChanged
-        contenido.modificado = True
+    Private Sub texto_TextChanged(sender As Object, e As EventArgs)
+        ListaArchivos(TabControl.SelectedIndex).modificado = True
+    End Sub
+    '!!'
+    Private Sub MyNotepad_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Dim i As Integer = 0
+        For Each b As Contenido In ListaArchivos
+            Me.TabControl.SelectedIndex = i
+            i = i + 1
+            If (b.modificado = True) Then
+                Dim resultado As MsgBoxResult = MsgBox("El archivo " + b.filename + " se ha modificado, ¿Desea guardarlo antes de salir?", vbCritical + vbYesNoCancel)
+                If (resultado = vbYes) Then
+                    SaveTool_Click(Nothing, Nothing)
+                End If
+                If (resultado = vbCancel) Then
+                    e.Cancel = True
+                End If
+            End If
+
+        Next
+
     End Sub
 
-    Private Sub MyNotepad_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
-        If (contenido.modificado = True) Then
-            Dim resultado As MsgBoxResult = MsgBox("El archivo se ha modificado, ¿Desea guardarlo antes de salir?", vbCritical + vbYesNoCancel)
-            If (resultado = vbYes) Then
-                SaveTool_Click(Nothing, Nothing)
-            End If
-            If (resultado = vbCancel) Then
-                e.Cancel = True
-            End If
+    Private Sub NewToolStripButton_Click(sender As Object, e As EventArgs) Handles NewToolStripButton.Click
+        NewTool_Click(sender, e)
+    End Sub
+
+    Private Sub OpenToolStripButton_Click(sender As Object, e As EventArgs) Handles OpenToolStripButton.Click
+        OpenTool_Click(Nothing, Nothing)
+    End Sub
+
+    Private Sub SaveToolStripButton_Click(sender As Object, e As EventArgs) Handles SaveToolStripButton.Click
+        SaveTool_Click(Nothing, Nothing)
+    End Sub
+
+    Private Sub TabControl_SelectedIndexChanged(sender As Object, e As EventArgs) Handles TabControl.SelectedIndexChanged
+        If TabControl.SelectedTab.Name = tpMas.Name Then
+            NewTool_Click(Nothing, Nothing)
+
         End If
     End Sub
 
-
+    Private Sub MyNotepad_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        NewTool_Click(Nothing, Nothing)
+    End Sub
 End Class
 
 
